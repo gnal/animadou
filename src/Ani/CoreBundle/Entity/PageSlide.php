21000e85d7797d55d6ba942744885026cc250ac8
Aleksis Joubert <qwertypianox@gmail.com>
2013-04-26 02:44:30 -0400

    fix

--- src/Ani/CoreBundle/Entity/PageSlide.php
+++ src/Ani/CoreBundle/Entity/PageSlide.php
@@ -54,7 +54,7 @@ class PageSlide implements TimestampableInterface, UploadableInterface
     {
         $cutter = new Cutter($file);
 
-        $cutter->resize(640, 200)->save();
+        $cutter->resize(640, 214)->save();
     }
 
     public function getPage()
