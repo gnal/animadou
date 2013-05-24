c4cbfb96adc5381172317c245df04ed1070d3e7c
Aleksis Joubert <qwertypianox@gmail.com>
2013-05-23 20:26:05 -0400

    fix

--- src/Ani/CoreBundle/Controller/AlbumController.php
+++ src/Ani/CoreBundle/Controller/AlbumController.php
@@ -9,7 +9,9 @@ class AlbumController extends Controller
     public function indexAction()
     {
         $albums = $this->get('ani_core.album_manager')->getFindByQueryBuilder(
-            ['a.published' => true]
+            ['a.published' => true],
+            ['a.photos' => 'p'],
+            ['p.position' => 'ASC']
         )->getQuery()->execute();
 
         return $this->render('AniCoreBundle:Album:index.html.twig', [
--- src/Ani/CoreBundle/Controller/PhotoController.php
+++ src/Ani/CoreBundle/Controller/PhotoController.php
@@ -3,16 +3,21 @@
 namespace Ani\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PhotoController extends Controller
 {
     public function indexAction()
     {
-        $album = $this->get('ani_core.album_manager')->getOneBy(
+        $album = $this->get('ani_core.album_manager')->getFindByQueryBuilder(
             ['a.published' => true],
             ['a.photos' => 'p'],
             ['p.position' => 'ASC']
-        );
+        )->getQuery()->getOneOrNullResult();
+
+        if (!$album) {
+            throw new NotFoundHttpException();
+        }
 
         return $this->render('AniCoreBundle:Photo:index.html.twig', [
             'album' => $album,
